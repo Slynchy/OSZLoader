0aab4817dbca1275668561045ce97431de495f6d
Sam Lynch <sam.lynch2@hotmail.co.uk>
2018-04-09 20:10:20 +0200

    Fully implement HitObject handling
    Barring Catmull/Bezier curve handling

--- index.js
+++ index.js
@@ -10,6 +10,54 @@ class OSULoader {
     constructor() {/* y u make instance :( */
     }
 
+    static get HitObjectTypes(){
+        return {
+            'circle': 0x1,
+            'slider': 0x2,
+            'new_combo': 0x4,
+            'spinner': 0x8,
+            'combo_col1': 0x10,
+            'combo_col2': 0x20,
+            'combo_col3': 0x40,
+            'osu!mania': 0x80
+        }
+    }
+
+    static get HitObjectSounds(){
+        return {
+            'normal': 0x1,
+            'whistle': 0x2,
+            'finish': 0x4,
+            'clap': 0x8,
+        }
+    }
+
+    static CreateHitObjectType(bitflags){
+        let object = {
+            isNewCombo: false,
+            isOsuMania: false,
+            type: undefined
+        };
+
+        if(bitflags & this.HitObjectTypes['circle']){
+            object.type = 'circle';
+        } else if(bitflags & this.HitObjectTypes['slider']){
+            object.type = 'slider';
+        } else if(bitflags & this.HitObjectTypes['spinner']){
+            object.type = 'spinner';
+        }
+
+        if(bitflags & this.HitObjectTypes['new_combo']){
+            object.isNewCombo = true;
+        }
+
+        if(bitflags & this.HitObjectTypes['osu!mania']){
+            object.isOsuMania = true;
+        }
+
+        return object;
+    }
+
     /**
      *
      * @param {Array<string>} splitFile The OSU file split into an array by newline
@@ -58,6 +106,20 @@ class OSULoader {
         }
     }
 
+    static GetHitSoundFromBitflag(bitflag){
+        if(bitflag & this.HitObjectSounds['normal']){
+            return 'normal';
+        } else if(bitflag & this.HitObjectSounds['whistle']){
+            return 'whistle';
+        } else if(bitflag & this.HitObjectSounds['finish']){
+            return 'finish';
+        } else if(bitflag & this.HitObjectSounds['clap']){
+            return 'clap';
+        } else {
+            return 'normal';
+        }
+    }
+
     static _handleColours(splitFile, index){
         let section = {};
 
@@ -80,7 +142,116 @@ class OSULoader {
         return section;
     }
 
+    static _getSampleSetType(sampleSet){
+        if(typeof(sampleSet) === 'undefined') throw new Error('Undefined addition set!');
+
+        switch(sampleSet){
+            default:
+            case 0:
+                return 'auto';
+            case 1:
+                return 'normal';
+            case 2:
+                return 'soft';
+            case 3:
+                return 'drum';
+        }
+    }
+
+    /**
+     * @param {string} string
+     * @private
+     */
+    static _handleExtras(data){
+        let extras = {};
+
+        // sampleSet:additionSet:customIndex:sampleVolume:filename
+        let splitStr = data.split(':');
+        extras['sampleSet'] = this._getSampleSetType(+splitStr[0]);
+        extras['additionalSet'] = (+splitStr[1]);
+        extras['customIndex'] = (+splitStr[2]);
+        extras['sampleVolume'] = (+splitStr[3]);
+        extras['filename'] = (splitStr[4]);
+
+        return extras;
+    }
+
+    static _getSliderType(typeStr){
+        switch(typeStr){
+            default:
+            case 'L':
+                return 'linear';
+            case 'P':
+                return 'perfect';
+            case 'B':
+                return 'bezier';
+            case 'C':
+                console.warn("Use of Catmull curve is deprecated");
+                return 'catmull';
+        }
+    }
+
+    /**
+     *
+     * @param {string} pathStr
+     * @private
+     */
+    static _handleSliderPath(pathStr){
+        let pathStrSplit = pathStr.split('|');
+
+        let path = {
+            sliderType: this._getSliderType(pathStrSplit[0])
+        };
+
+        switch(path.sliderType){
+            case 'linear':
+                // L|100:200
+                path['end'] = {
+                    x: (+pathStrSplit[1].split(':')[0]),
+                    y: (+pathStrSplit[1].split(':')[1])
+                };
+                break;
+            case 'perfect':
+                path['passthrough'] = {
+                    x: (+pathStrSplit[1].split(':')[0]),
+                    y: (+pathStrSplit[1].split(':')[1])
+                };
+                path['end'] = {
+                    x: (+pathStrSplit[2].split(':')[0]),
+                    y: (+pathStrSplit[2].split(':')[1])
+                };
+                break;
+            case 'bezier':
+            case 'catmull':
+                // TODO: bezier and catmull curves
+                console.warn('Bezier and catmull curves not yet implemented');
+                break;
+        }
+
+        return path;
+    }
+
+    static _handleSlider(splitLine, entryRef){
+        // x,y,time,type,hitSound,sliderType|curvePoints,repeat,pixelLength,edgeHitsounds,edgeAdditions,extras
+
+        entryRef['path'] = this._handleSliderPath(splitLine[5]);
+        entryRef['repeat'] = (+splitLine[6]);
+        entryRef['pixelLength'] = (+splitLine[7]);
+        entryRef['edgeHitsounds'] = (splitLine[8].split('|'));
+
+        if(entryRef['edgeHitsounds'].length !== entryRef['repeat']+1){
+            throw new Error('Number of edge hitsounds does not match repeat+1 syntax!');
+        }
+        for(let i = 0; i < entryRef['repeat']+1; i++){
+            entryRef['edgeHitsounds'][i] = (+entryRef['edgeHitsounds'][i]);
+        }
+
+        return entryRef;
+    }
+
     static _handleHitObjects(splitFile, index){
+        let section = [];
+
         let currLine;
         while ((currLine = splitFile[++index]).trim() !== '') {
             //x,y,time,type,hitSound...,extras
@@ -89,10 +260,40 @@ class OSULoader {
                 x: +currLineSplit[0],
                 y: +currLineSplit[1],
                 time: +currLineSplit[2],
-                type: +currLineSplit[3],
-                hitSound: +currLineSplit[4]
+                type: this.CreateHitObjectType(+currLineSplit[3]),
+                hitSound: this.GetHitSoundFromBitflag(+currLineSplit[4]),
+                extras: undefined
             };
+
+            // regular circle
+            switch(entry.type.type){
+                case 'circle':
+                    // x,y,time,type,hitSound,extras
+                    if(currLineSplit.length === 6){
+                        entry['extras'] = this._handleExtras(currLineSplit[5]);
+                    }
+                    break;
+                case 'slider':
+                    entry = this._handleSlider(currLineSplit, entry);
+                    if(currLineSplit.length === 11){
+                        entry['extras'] = this._handleExtras(currLineSplit[10]);
+                    }
+                    break;
+                case 'spinner':
+                    //x,y,time,type,hitSound,endTime,extras
+                    entry['endTime'] = (+currLineSplit[10]);
+                    if(currLineSplit.length === 7){
+                        entry['extras'] = this._handleExtras(currLineSplit[6]);
+                    }
+                    break;
+                case 'new_combo': // ??
+                    break;
+            }
+
+            section.push(entry);
         }
+
+        return section;
     }
 
     static _getFileVersion(str) {
